9c203b64ac712ac8ae67f66559b740444e76cc58
Ibrahim Tamimi <ibrahimissam3@gmail.com>
2020-10-06 19:21:55 +0100

    Create insert_pageviews_postcode_history_count.sql
    insert new data to pageviews_postcode_history_count

---
+++ insert_pageviews_postcode_history_count.sql
@@ -0,0 +1,15 @@
+insert into  
+bi_analytic.pageviews_postcode_history_count
+(date ,   
+ hour ,
+postcode ,
+pageviews_count)                      
+ select
+puh.pageview_datetime :: date ,
+hour (puh.pageview_datetime) ,
+puh.postcode,
+count(puh.*) -- assume pageview is count for any page from any user
+from  bi_analytic.Pageviews_user_history as puh
+where puh.pageview_datetime :: date >= %(date_cutoff)s
+and   hour(puh.pageview_datetime) >= %(hour_cutoff)s
+group by 1 , 2, 3
